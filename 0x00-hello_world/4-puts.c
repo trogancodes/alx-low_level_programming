373a369be8b7e961aa96e2fac0edc1956721c665
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-15 12:18:50 -0700

    y

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a line of code using puts
- * Return: always 0
+ * main - A program that prints a line with puts function
+ * Return: 0 
  * /
-  int main(void)
-   { 
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-   }
+int main(void)
+{ 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
