7a181c94c8ab09c2f357a901cf8605eea96bbb2a
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-16 13:02:03 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+	return (0);
+}
