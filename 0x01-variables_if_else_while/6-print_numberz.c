1489cdc06030b7c34c25226aa54868c0af7ee1a8
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-16 13:16:57 -0700

    n

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n < 0; n++)
+	{
+	putchar(n + 48);
+	putchar('\n');
+	}
+	return (0);
+}
