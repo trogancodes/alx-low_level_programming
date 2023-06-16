cb8d5cc1fe789dd833bfd4b5480e1ec45d7a35e2
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-16 11:49:10 -0700

    n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,23 +8,24 @@
  */
 int main(void)
 {
-	int n, lastd;
+	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
+	ld = n % 10;
 
-	if (lastd > 5)
+	if (ld > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if
+	else if (ld == 0)
 	{
-	prinf("Last digit of %d is %d and is 0\n", n, last);
+	printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
-	else if ("Lastd < 6 && lastd != 0")
+	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not o\n", n, lastd);
+	printf("Last digit of %d is %d and is less than 6 and not o\n", n, ld);
 	}
 	return (0);
 }
