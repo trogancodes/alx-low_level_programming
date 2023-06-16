040aa9182730d7578409cb9d56b558d4ce368d07
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-16 13:58:01 -0700

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+	putchar(n);
+	if (n == 57)
+	{
+	continue;
+	}
+	putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
