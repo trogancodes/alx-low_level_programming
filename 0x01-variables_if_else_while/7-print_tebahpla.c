9510a2fb092a7d3bf55eed7f131d51d4e776167f
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-16 13:31:56 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+	putchar(n);
+	n--;
+	}
+	putchar('\n');
+	return (0);
+}
