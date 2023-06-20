e69ef825c436fdd4937ead4fb6bad62c3456f777
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-20 14:06:11 -0700

    g

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - E
+ * less than 4000000.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + l < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += ik;
+	j = k - j; i
+	++i;
+	}
+	printf("%ld\n", sum)
+	return (0);
+}
