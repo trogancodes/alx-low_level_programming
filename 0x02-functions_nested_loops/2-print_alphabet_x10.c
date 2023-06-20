33fab7e807b4bc444f1f53073bcbee54e5b171ad
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-20 07:35:13 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -Make alphabet x10 time
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
