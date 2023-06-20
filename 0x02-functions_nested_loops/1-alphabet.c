df724f6574360874429953120c644230f3a11b38
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-20 06:36:57 -0700

    v

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,11 @@
  * print_alphabet - Make the alphabet
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
-	
+
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
