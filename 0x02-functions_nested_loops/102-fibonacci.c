02aa6ac947013ebd752be0c2eaff096bfe9d8e27
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-20 13:49:56 -0700

    o

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - E
+ * Return: Nothing!
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+	if (i == 0)
+	printf("%ld", j);
+	else is (i == 1)
+	printf(", %ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(",%ld", k);
+	}
+	++i;
+	}
+	printf("\n");
+	return (0);
+}
