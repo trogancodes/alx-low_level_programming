f4e5b323bcf66c4fcbd364e06f3032deef5920aa
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-20 06:08:48 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int abs(int);
+int print_last_digit(int);
+int jack_bauer(void);
+int times_table(void);
+int add(int, int);
+int print_to_98(int n);
+void print_times_table(int n);
