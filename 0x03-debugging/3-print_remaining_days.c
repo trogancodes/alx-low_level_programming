913d1c74bb41f271af6433b0131f156f11eb2b67
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-21 01:28:36 -0700

    j

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 	if (month == 2 && day == 60)
 	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
@@ -30,4 +30,4 @@ void print_remaining_days(int month, int day, int year)
 	printf("Remaining days: %d\n", 365 -  day);
 	}
 	}
-}	
+}
