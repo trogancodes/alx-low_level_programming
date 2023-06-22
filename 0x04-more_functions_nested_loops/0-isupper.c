ea424cf14179b8741869cf7b71c48ce45e0a6a2f
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 07:40:55 -0700

    y

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Check
+ * @x: The
+ * Return: 1 for upper letter or o for any else
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
