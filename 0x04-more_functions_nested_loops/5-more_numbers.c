653ea0bf0ef8cf620b10ed60786e0443ba024a7f
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 08:05:01 -0700

    p

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - b
+ * Return: 10 times of the number since 0 up to 14
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y < 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y / 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
