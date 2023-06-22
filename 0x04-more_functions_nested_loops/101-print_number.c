4ae05825cb9e040dbf4064520cabddd43ae6d31a
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 13:54:21 -0700

    p

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * print_number - kj
+ * @n: fhgjkjlk
+ * Return: Nothing!
+ */
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+	n *= -1;
+	k = n;
+	_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+	{
+	print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+	}
+}
