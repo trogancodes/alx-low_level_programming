e9d6a803f4e4db5d0f86a46fed29710e651f4b7c
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 08:54:24 -0700

    p

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - jj
+ * @size: jhgchgjkl;
+ * Return: empty
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (z = size - x; y > 1; y--)
+	}
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar((35);
+	}
+	_putchar("\n");
+	}
+	}
+}
