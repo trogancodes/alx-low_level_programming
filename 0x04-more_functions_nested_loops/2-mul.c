a5ae45cde66ba7dd73d1e675e1f458c7d820a1ba
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 07:49:06 -0700

    o

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - mult
+ * @x: ffirs
+ * @y: secs
+ * Return: The result of the multiplication between x param and y param
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
