38c9c909e78b3bfcf1ceebb21b3fc4d809acbfc8
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 07:45:40 -0700

    p

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - C
+ * @x: t
+ * Return: 1 for a character that would be a digit or 0 for any else
+ */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
