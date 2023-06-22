fac8a7231782a8340c21c6e03a9ddafb72a70b72
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-22 08:10:53 -0700

    g

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - k
+ * @n: bb
+ * Return: empty
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
