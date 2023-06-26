b64a70073dc38e9298ac5e10f4f34ea3f3443877
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-26 07:38:17 -0700

    h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * swap_int - vk
+ * @a: ghjkl
+ * @b: jbv
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+/* the function that swaps the value of two integers. */
+{
+	int temp =*a;
+	*a = *b;
+	*b = temp;
+}
