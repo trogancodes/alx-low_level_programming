5536272292be6f701918641e0dcf430f14692d4e
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-26 07:45:23 -0700

    i

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _strlen - vcvhjk
+ * @str: bnm,l;
+ * Return: thnnbbv
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (str++)
+	length++;
+	return (length);
+}
