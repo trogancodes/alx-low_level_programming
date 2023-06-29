8813725864d8af103bc6693b930bfdced6ec7565
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-06-29 09:04:33 -0700

    hjkl;

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncat - fhkl
+ * @dest: ghjkl
+ * @src: fghjkl
+ * @n: ghjm
+ * Return: A pointer to the resulting string dest;
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	dest_len++;
+	for (index = 0; src[index++] && index < n; index++)
+	dest[dest_len++] = src[index];
+	return (dest);
+}
