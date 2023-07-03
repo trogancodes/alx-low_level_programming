3159bc0160e07925fade9db702750b3d3b132ad1
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-03 15:17:15 -0700

    sdfgh

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - hdjc
+ * @a: dfgh
+ * @size: xcv
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, n, sum1 = 0, sum2 = 0;
+	for (i = 0; i <= (size * size); i = i + size + 1)
+	sum1 =sum1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
+}
