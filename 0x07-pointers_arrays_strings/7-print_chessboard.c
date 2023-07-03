34a00980c9e62bb4d49057217f27b7f243eb0e6c
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-03 15:03:36 -0700

    sdfg

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_chessboard - jdwnx
+ * @a: dfghj
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+	for (i = 0; i < 0; i++)
+	{
+	for (n = 0; n < 0; n++)
+	_putchar(a[i][n]);
+	_putchar('\n');
+	}
+}
