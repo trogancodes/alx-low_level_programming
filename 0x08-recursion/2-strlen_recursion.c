f7e3934dd2c49205c0d783dbf47016468bb53d24
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-05 05:27:34 -0700

    fghjk

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - ertyu
+ * @s: sdfgh
+ * Return: sdfg
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
