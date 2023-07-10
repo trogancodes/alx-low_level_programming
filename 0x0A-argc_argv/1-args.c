9b8352c8b4496710280ad2186c22bad4e051d785
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-10 01:17:04 -0700

    vbd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints some
+ * @argc: count
+ * @argv: vector
+ * Return: Always zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%\n", argc - 1);
+	return (0);
