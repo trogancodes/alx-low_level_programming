b9161a16086990d8b9a149f1e71c1049ea150668
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-10 03:22:29 -0700

    sdfgh

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - fghj
+ * @argc: sdfg
+ * @argv: sdfghjk
+ * Return: 1 0r 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+	if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+	coins++;
+	if ((cents - 25) >= 0)
+	{
+	cents -= 25;
+	continue;
+	}
+	if ((cents - 10) >= 0)
+        {
+        cents -= 10;
+        continue;
+        }
+	if ((cents - 5) >= 0)
+        {
+        cents -= 5;
+        continue;
+        }
+	if ((cents - 2) >= 0)
+        {
+        cents -= 2;
+        continue;
+        }
+	cents--;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
