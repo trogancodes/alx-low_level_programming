8f53ea4193fd510c162693197883d7b9c455a67d
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-10 01:40:08 -0700

    nc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - fgh
+ * @argc: count
+ * @arg: vctor
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for(i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
