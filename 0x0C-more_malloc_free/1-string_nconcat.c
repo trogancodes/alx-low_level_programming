394b5c6e3a802d18d6b94b2b0541e98928a91eb2
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-13 06:48:23 -0700

    dfghjk

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include" main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked  - cghjk
+ * @b: ghjk
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == 0)
+		exit(98);
+	return (p)
+}
