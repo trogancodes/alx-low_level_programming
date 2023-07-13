922dccf1cee847cbab95e0c59de10d6f9524ad79
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-13 07:11:02 -0700

    dfghj

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,18 +1,44 @@
-#include" main.h"
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * malloc_checked  - cghjk
- * @b: ghjk
- * Return: Nothing
+ * string_nconcat - dfgh
+ * @s1: sdfghj
+ * @s2: sdfghjk
+ * @n:asdfghjk
+ * Return:zxc
  */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *p;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	p = malloc(b);
-
-	if (p == 0)
-		exit(98);
-	return (p)
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+	str = malloc(sizeof(char) * l + 1);
+	if (str == 0)
+		return (0);
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
