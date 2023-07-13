42395a4adfb7c74b4bdb6e4bc41e1981e94b9f4e
trogancodes <chiedoziemaduabuchi@gmail.com>
2023-07-13 06:53:06 -0700

    fghj

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,4 @@
-#include" main.h"
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -14,5 +14,5 @@ void *malloc_checked(unsigned int b)
 
         if (p == 0)
                 exit(98);
-        return (p)
+        return (p);
 }
